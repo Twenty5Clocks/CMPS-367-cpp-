0c16ee81d1b81b6eda51f476610e4e3bd7370976
Twenty5Clocks <JTDriesler@gmail.com>
2015-02-13 16:27:57 -0800

    added week 2 prob 1 completion

--- Week2Prob1/Week2Prob1/Prob1.cpp
+++ Week2Prob1/Week2Prob1/Prob1.cpp
@@ -1,11 +1,12 @@
-//------------------------------------
+﻿//------------------------------------
 // Name: Jeremy Driesler
 // Course: CMPS 367
 // Project: No2 Part 1
 // Due: February 23, 2015
 // Professor: Ray Ahmadnia
 //
-//Purpose: 
+//Purpose: Output the dollar to yen, pound and euro rates
+//for a given range and increment
 //------------------------------------------
 
 
@@ -23,6 +24,11 @@ double underlines				char(205)
 
 using namespace std;
 
+const float DOLLAR2YEN = 119.45;
+const float DOLLAR2POUND = 0.66;
+const float DOLLAR2EURO = 0.88;
+
+
 int main(){
 
 //declare variables
@@ -31,7 +37,7 @@ int startVal, finalVal, incVal;
 
 
 //user inputs data
-cout << "Please enter the table starting value, final value and increment value: ";
+cout << "Please enter the table starting value," << endl <<"final value and increment value: ";
 cin >> startVal >> finalVal >> incVal;
 
 //Draw the table
@@ -40,6 +46,12 @@ cout << right << setw(6) << char(36) << right << setw(10) << char(157) << right 
 	<< right << setw(10) << char(238) << endl;
 for (int i = 0; i <=40; i++) cout << char(205);
 cout << endl;
+
+//display values in table
+for(int i = startVal; i <= finalVal; i += incVal){
+	cout << right << setw(8) << (float)i << right << setw(10) << (float)i *DOLLAR2YEN << right << setw(10) << (float)i*DOLLAR2POUND
+		<< right << setw(10) << (float)i*DOLLAR2EURO << endl;
+}
 
 
 
@@ -50,7 +62,19 @@ return 0;
 
 }
 /*-------------------------------output-------------------------
-Enter today's date:2/2/2015
-Today is February 2nd, 2015
+Please enter the table starting value,
+final value and increment value: 10 100 10
+     $         ¥         £         ε
+═════════════════════════════════════════
+   10.00   1194.50      6.60      8.80
+   20.00   2389.00     13.20     17.60
+   30.00   3583.50     19.80     26.40
+   40.00   4778.00     26.40     35.20
+   50.00   5972.50     33.00     44.00
+   60.00   7167.00     39.60     52.80
+   70.00   8361.50     46.20     61.60
+   80.00   9556.00     52.80     70.40
+   90.00  10750.50     59.40     79.20
+  100.00  11945.00     66.00     88.00
 Press any key to continue . . .
 ----------------------------------------------------------------*/
